015488fe8bc0e19a7f7a5d7426968ae8f17334b4
Manu <M.laux@wooobee.com>
2013-04-23 11:56:14 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobilOrder::Application.config.secret_token = '2d6586f2bf94fe3548ae013c1d11afe8e257889cdc2a4fe5958c0ad64fbe008193eaae354188cecd202db0bb9d3ea90b9be0943cd5413a23e315e4bd9fbb4705'
