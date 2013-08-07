c410e166547bc44976abc78f4285dbabd87810d4
Manu <M.laux@wooobee.com>
2013-08-07 22:21:18 +0200

    oeffnungszeiten added

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
   attr_accessible :city, :email, :forname, :mobile, :name, :order_type, :street, :zip, :pickup_time
   
-  validates :name, :email, :order_type, :presence => true
+  validates :name, :email, :order_type, :mobile, :presence => true
   
   def add_line_items_from_cart(cart) 
     cart.line_items.each do |item|
